13c8d9ec88366697fa91fe894051e6ec2c4973b4
Michael Pori <michael.pori@russellcellular.com>
2023-12-04 15:00:33 -0600

    Trying to get current directory.

--- 2023/Day1/Rust/trebuchet_calibration/src/main.rs
+++ 2023/Day1/Rust/trebuchet_calibration/src/main.rs
@@ -1,3 +1,8 @@
+use std::fs::File;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let environment_path_result = env::current_dir();
+
+    //let calibration_file_result = File::open(env::current_dir());
 }
