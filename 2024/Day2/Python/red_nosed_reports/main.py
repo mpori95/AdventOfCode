c0c4168b1abfbbe0923bdca7f2ceff5feef18cbb
Michael Pori <michael.pori@russellcellular.com>
2025-06-09 15:41:44 -0500

    Started day 2

---
+++ 2024/Day2/Python/red_nosed_reports/main.py
@@ -0,0 +1,11 @@
+def main():
+    print("js sucks")
+
+def collect_reports():
+
+    with open("unusual_data.txt", "r") as file:
+        for line in file:
+            line.strip().split()
+   
+if __name__ == "__main__":
+    main()
---
+++ 2024/Day2/Python/red_nosed_reports/report.py
@@ -0,0 +1,4 @@
+class Report:
+    def __init__(self, value: str, is_safe: bool):
+        self.value = value
+        self.is_safe = is_safe
