264c33b812b7d3de87dfce67f869e23c4ab98bfd
Michael Pori <michael.pori@russellcellular.com>
2025-06-09 12:00:17 -0500

    Fixed directory structure.

---
+++ 2024/Day1/Python/historian_hysteria/column_data.py
@@ -0,0 +1,4 @@
+class ColumnData:
+    def __init__(self, first_column: list[str], second_column: list[str]):
+        self.first_column = first_column
+        self.second_column = second_column
--- 2024/Day1/Python/historian_hysteria/main.py
+++ 2024/Day1/Python/historian_hysteria/main.py
@@ -1,7 +1,17 @@
+from column_data import ColumnData
+
 def main():
-    collect_location_ids()
+    column_data = collect_location_ids()
+    # print_location_id_differences(column_data)
+    print_left_list_similarities(column_data)
 
 def collect_location_ids():
+    """
+    Collects location ids from the text file.
+
+    Returns:
+        ColumnData: The class containing the location id data.
+    """
 
     first_column = []
     second_column = []
@@ -15,19 +25,46 @@ def collect_location_ids():
     first_column.sort()
     second_column.sort()
 
-    column_length = len(first_column)
+    return ColumnData(first_column, second_column)
+
+def print_location_id_differences(column_data: ColumnData):
+    """
+    Prints the location id defferences between the two columns.
+
+    Args:
+        column_data (ColumnData): The column data containing the location ids.
+    """
+
+    column_length = len(column_data.first_column)
 
     total_difference = 0
 
     for i in range(column_length):
-        left_value = int(first_column[i])
-        right_value = int(second_column[i])
+        left_value = int(column_data.first_column[i])
+        right_value = int(column_data.second_column[i])
 
         difference = left_value - right_value if left_value > right_value else right_value - left_value
 
         total_difference += difference
 
     print(total_difference)
+
+
+def print_left_list_similarities(column_data: ColumnData):
+    """
+    Prints the similarity score of the left list.
+
+    Args:
+        column_data (ColumnData): The column data containing the location ids.
+    """
+
+    total_similarity_score = 0
+
+    for item in column_data.first_column:
+        number_of_occurances = column_data.second_column.count(item)
+        total_similarity_score += int(item) * number_of_occurances
+
+    print(total_similarity_score)
 
 if __name__ == "__main__":
     main()
