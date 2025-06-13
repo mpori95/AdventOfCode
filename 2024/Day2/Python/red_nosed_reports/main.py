3cc02e2bd4cbeccc1314375e809b9e0d3fed9799
Michael Pori <michael.pori@russellcellular.com>
2025-06-13 14:18:09 -0500

    Push

--- 2024/Day2/Python/red_nosed_reports/main.py
+++ 2024/Day2/Python/red_nosed_reports/main.py
@@ -1,11 +1,40 @@
+from report import Report
+
 def main():
-    print("js sucks")
+    collect_reports()
 
 def collect_reports():
 
+    safe_reports = 0
+
+    reports = []
+
     with open("unusual_data.txt", "r") as file:
-        for line in file:
-            line.strip().split()
+        for report in file:
+            levels = report.strip().split()
+
+            max_difference = 3
+
+            is_increasing = all(
+                is_pair_increasing(int(level), int(next_level), max_difference)
+                  for level, next_level in zip(levels, levels[1:]))
+            
+            is_decreasing = all(
+                is_pair_decreasing(int(level), int(next_level), max_difference)
+                  for level, next_level in zip(levels, levels[1:]))
+            
+            if (is_increasing or is_decreasing):
+                reports.append(Report(int(levels), True))
+                
+
+        
+        print(safe_reports)
+            
+def is_pair_increasing(level: int, next_level: int, max_difference: int):
+    return level < next_level and next_level - level <= max_difference
+
+def is_pair_decreasing(level: int, next_level: int, max_difference: int):
+    return level > next_level and level - next_level <= max_difference
    
 if __name__ == "__main__":
     main()
--- 2024/Day2/Python/red_nosed_reports/report.py
+++ 2024/Day2/Python/red_nosed_reports/report.py
@@ -1,4 +1,6 @@
+from typing import List
+
 class Report:
-    def __init__(self, value: str, is_safe: bool):
-        self.value = value
+    def __init__(self, levels: List[int], is_safe: bool):
+        self.levels = levels
         self.is_safe = is_safe
